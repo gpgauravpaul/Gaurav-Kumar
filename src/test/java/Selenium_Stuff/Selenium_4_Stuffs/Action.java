77cccaf7fa3791cbd8894eb8ad58b980dd4055ad
gaurav.paul <gaurav.paul@outlook.in>
2020-04-20 16:08:18 +0530

    love

--- src/test/java/Selenium_Stuff/Selenium_4_Stuffs/Action.java
+++ src/test/java/Selenium_Stuff/Selenium_4_Stuffs/Action.java
@@ -1,31 +1,38 @@
 package Selenium_Stuff.Selenium_4_Stuffs;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Action {
 
-	
 	public static WebDriver driver ; 
 	
-	public static void main(String[] args) {
-		
-		
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/gaurav.pauloutlook.in/Downloads/chromedriver");
+	public static void main(String[] args) throws IOException, InterruptedException {
+			
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.amazon.in/");
-		// driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		WebElement element = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
-		
-		Actions act = new Actions(driver);
-		act.moveToElement(element).click().keyDown(Keys.SHIFT).sendKeys("test").doubleClick(element).build().perform();
-		
-	}
+		driver.get("https://altoconvertpdftojpg.com/");
+		driver.findElement(By.xpath("//*[@id=\"dropzoneInput-label\"]")).click();
+		Thread.sleep(2000);
+       Runtime.getRuntime().exec("C:\\Users\\saura\\Desktop\\FileUploadSection");
+       Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@contains(@class,'g-btn--primary')]")).click();
+        WebElement element = driver.findElement(By.xpath("//*[text()='Congratulations! Your Document is Ready!']"));
+         WebDriverWait wait = new WebDriverWait(driver,10);
+         wait.until(ExpectedConditions.visibilityOf(element));
+         driver.findElement(By.xpath("outputResultComponent--button")).click();
+         
+         
 	
-}
+	
+	
+	}}
