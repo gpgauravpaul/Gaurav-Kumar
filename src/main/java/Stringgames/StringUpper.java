52288263946d6594d47c74cb781036fd287a1237
gpgauravpaul <56973726+gpgauravpaul@users.noreply.github.com>
2020-11-24 16:41:24 +0530

    StringUpper

--- src/main/java/Stringgames/StringUpper.java
+++ src/main/java/Stringgames/StringUpper.java
@@ -1,2 +1,12 @@
-package Stringgames;public class StringUpper {
+package Stringgames;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+public class StringUpper {
+    public static void main (String[] args) {
+        String str = "my name is gaurav";
+
+        System.out.println (str.toUpperCase ());
+
+    }
 }
