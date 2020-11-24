9905b44b61ecc7ed37cf8f7faf5d662981b69625
gpgauravpaul <56973726+gpgauravpaul@users.noreply.github.com>
2020-11-24 16:40:36 +0530

    Deleted one

---
+++ src/main/java/Stringgames/FirstCharacterCaps.java
@@ -0,0 +1,26 @@
+package Stringgames;
+
+import java.util.Scanner;
+
+public class FirstCharacterCaps {
+
+    public static void main (String[] args) {
+        System.out.println ("Enter a String");
+        Scanner scan = new Scanner(System.in);
+        System.out.println (func (scan.nextLine ()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static String func (String str) { String split[] = str.split (" "); String Main ="";int count = split.length;for(String word:split) { int wordcount = word.length ();String blank = ""; for (int j=0;j<wordcount;j++) { if (j==0) { blank = blank + Character.toUpperCase (word.charAt (j)); } else { blank = blank + word.charAt(j); } }Main = Main +" "+ blank; }return  Main; }}
---
+++ src/main/java/Stringgames/StringUpper.java
@@ -0,0 +1,2 @@
+package Stringgames;public class StringUpper {
+}
